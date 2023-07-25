5d20f98e742fdeccacc005b347aa90bdcad76b1e
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-07-25 13:59:53 +0000

    commit all

--- index.php
+++ index.php
@@ -51,6 +51,11 @@ try {
 	echo "L'erreur " . $e->getMessage() . " est gérée localement" . PHP_EOL;
 }
 echo 'Le programme continue ici' . PHP_EOL;
+
+// On peut déclarer un gestionnaire d'erreurs global pour gérer globalement les erreurs émises par nos fonctions PHP. Un gestionnaire d'erreurs global se déclare avec la fonction PHP 
+set_error_handler($callable $callback, int $error_levels): ?callable. 
+
+  //Il vous permet de personnaliser la génération de vos rapports en enregistrant une fonction $callback. Cette fonction sera appelée lorsqu’une erreur sera émise dans votre code. Vous pouvez ainsi, comme dans les blocs catch, exécuter du code personnalisé (par exemple, envoyer un e-mail à l'administrateur du site, puis tuer le programme).
 ?>
   
   <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"></script>
